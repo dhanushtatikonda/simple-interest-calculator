f5cf194e9c69ee84c995dc260f7b2460d92649ea
Tatikonda Dhanush <tatikondadhanush@gmail.com>
2025-08-19 23:00:42 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# Simple Interest Calculator
+
+# Input
+echo "Enter Principal:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+
+# Calculation
+interest=$(( (principal * rate * time) / 100 ))
+
+# Output
+echo "Simple Interest is: $interest"
 
